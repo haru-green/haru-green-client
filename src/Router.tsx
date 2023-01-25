fe1ec1020ca4d1f6d50fc0957006786badad7334
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-25 15:22:19 +0900

    refactor: import alias 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Quiz from './pages/Quiz';
-import Result from './pages/Result';
-import Root from './Root';
+import Home from '@/pages/Home';
+import Login from '@/pages/Login';
+import Quiz from '@/pages/Quiz';
+import Result from '@/pages/Result';
+import Root from '@/Root';
 
 const Router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <Root />,
     children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
       {
         path: '/login',
         element: <Login />,
