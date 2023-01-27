a8ee31395e7dbbd34e9a8999b751deb5737322ed
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-27 16:39:10 +0900

    feat: 공통 모달 컴포넌트 생성

--- src/shared/Modal/Modal.tsx
+++ src/shared/Modal/Modal.tsx
@@ -1,3 +1,25 @@
-const Modal = () => {};
+import classNames from 'classnames/bind';
+
+import CloseIcon from '@/assets/icons/close.svg';
+
+import styles from './Modal.module.scss';
+
+const cx = classNames.bind(styles);
+
+type ModalProps = {
+  closeModal: () => void;
+};
+
+const Modal = ({ closeModal }: ModalProps) => {
+  return (
+    <div onClick={closeModal} className={cx('modalOverlay')}>
+      <div className={cx('modal')}>
+        <button onClick={closeModal} className={cx('closeButton')}>
+          <img alt="close" src={CloseIcon} />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
