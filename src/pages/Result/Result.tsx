40a850c1015bf46ac4fc319e2dcf035a62c7dedc
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-02-01 00:11:14 +0900

    feat: Result 페이지 구현

--- src/pages/Result/Result.tsx
+++ src/pages/Result/Result.tsx
@@ -1,31 +1,44 @@
 import classNames from 'classnames/bind';
+import { useState } from 'react';
 
-import StartQuizButton from '@/features/Home/StartQuizButton';
+import ModalContents from '@/features/Result/ModalContents';
 import Score from '@/features/Result/Score';
+import Modal from '@/shared/Modal';
 import NavigateButton from '@/shared/NavigateButton';
+import OutlinedButton from '@/shared/OutlinedButton';
 
 import styles from './Result.module.scss';
 
 const cx = classNames.bind(styles);
 
 const Result = () => {
+  const [isAnswerModalOpen, setIsAnswerModalOpen] = useState<boolean>(false);
+
+  const openModal = () => setIsAnswerModalOpen(true);
+  const closeModal = () => setIsAnswerModalOpen(false);
+
   return (
-    <div className={cx('wrapper')}>
-      <Score score="three" />
-      <div className={cx('result')}>
-        <p>두부님은 총</p>
-        <p className={cx('score')}>
-          <span>0문제</span>
-          <span> 맞췄어요</span>
-        </p>
-        <StartQuizButton
-          onClick={() => {
-            console.log('hi');
-          }}
-        />
+    <>
+      <div className={cx('wrapper')}>
+        <Score score="three" />
+        <div className={cx('result')}>
+          <p>두부님은 총</p>
+          <p className={cx('score')}>
+            <span>0문제</span>
+            <span> 맞췄어요</span>
+          </p>
+          <OutlinedButton text="정답 보러가기" onClick={openModal} />
+        </div>
+        <NavigateButton text="홈으로" destination="/" />
       </div>
-      <NavigateButton text={'홈으로'} />
-    </div>
+      {isAnswerModalOpen && (
+        <Modal
+          closeModal={closeModal}
+          title="정답"
+          content={<ModalContents />}
+        />
+      )}
+    </>
   );
 };
 
