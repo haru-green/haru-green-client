142c3ed48f3b83d7aaf4d0f1c77e07cc375d1e1d
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-31 14:43:56 +0900

    feat: Quiz 버튼 구현

---
+++ src/features/Quiz/Button/Button.tsx
@@ -0,0 +1,33 @@
+import classNames from 'classnames/bind';
+
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+type ButtonProps = {
+  text: string;
+  isCorrectBtnClicked?: boolean;
+  isWrongBtnClicked?: boolean;
+  onClick?: () => void;
+};
+
+const Button = ({
+  text,
+  isCorrectBtnClicked,
+  isWrongBtnClicked,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cx(
+        'button',
+        (isCorrectBtnClicked || isWrongBtnClicked) && 'clicked'
+      )}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
