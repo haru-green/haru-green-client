70759ed78ce49b2e14f47a809ca7f2bc43d5d8f9
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-02-28 14:31:58 +0900

    feat: 로그인 돼있으면 홈으로 이동

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import axios from '@/api/axios';
 import loginWithAuthCode from '@/api/loginWithAuthCode';
 import { Logo } from '@/assets/icons';
 import KaKaoLogin from '@/assets/images/kakaoLogin.png';
@@ -9,13 +10,19 @@ import styles from './Login.module.scss';
 
 const cx = classNames.bind(styles);
 
-const kakaoLogin = () => {
-  window.location.href = `${process.env.REACT_APP_KAKAO_AUTH_URI}&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}`;
-};
+const kakaoLogin = () =>
+  (window.location.href = `${process.env.REACT_APP_KAKAO_AUTH_URI}&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}`);
 
 const Login = () => {
+  const navigate = useNavigate();
   const kakaoAuthCode = new URLSearchParams(window.location.search).get('code');
   if (kakaoAuthCode) loginWithAuthCode(kakaoAuthCode);
+
+  useEffect(() => {
+    if (document.cookie.includes('auth_token')) {
+      navigate('/');
+    }
+  }, []);
 
   return (
     <main className={cx('main')}>
