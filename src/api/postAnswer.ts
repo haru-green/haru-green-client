2a235e04f4803a5e5560e569766b29cc3da61552
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-03-02 00:11:59 +0900

    refactor: postAnswer 로직 변경

--- src/api/postAnswer.ts
+++ src/api/postAnswer.ts
@@ -1,9 +1,7 @@
 import axios from '@/api/axios';
+import { getToken } from '@/util';
 
 const postAnswer = async (email: string | undefined, userAnswer: boolean[]) => {
-  const authToken = document.cookie.split(';')[1].split('auth_token=')[1];
-  const refreshToken = document.cookie.split(';')[2].split('auth_refresh=')[1];
-  const authGrant = document.cookie.split(';')[0].split('auth_grant=')[1];
   await axios.post(
     `/quiz/try?email=${email}`,
     {
@@ -13,9 +11,9 @@ const postAnswer = async (email: string | undefined, userAnswer: boolean[]) => {
     },
     {
       headers: {
-        'X-AUTH-TOKEN': authToken,
-        'X-AUTH-REFRESH': refreshToken,
-        'X-AUTH-GRANT': authGrant,
+        'X-AUTH-TOKEN': getToken().authToken,
+        'X-AUTH-REFRESH': getToken().refreshToken,
+        'X-AUTH-GRANT': getToken().authGrant,
       },
     }
   );
