3b29afd5db5ee471c8e40aae849bac7346e3042b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-03-02 12:29:45 +0900

    feat: ErrorElement 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import Login from '@/pages/Login';
 import Quiz from '@/pages/Quiz';
 import Result from '@/pages/Result';
 import Root from '@/Root';
+import ErrorElement from '@/shared/ErrorElement';
 
 const Router = createBrowserRouter([
   {
@@ -14,18 +15,26 @@ const Router = createBrowserRouter([
       {
         path: '/login',
         element: <Login />,
+        errorElement: <ErrorElement />,
       },
       {
         path: '/quiz/:id',
         element: <Quiz />,
+        errorElement: <ErrorElement />,
       },
       {
         path: '/result',
         element: <Result />,
+        errorElement: <ErrorElement />,
       },
       {
         path: '/',
         element: <Home />,
+        errorElement: <ErrorElement />,
+      },
+      {
+        path: '*',
+        element: <ErrorElement />,
       },
     ],
   },
