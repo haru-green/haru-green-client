4a3c2118583f213f68b1e65229fc8223c3f4d897
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-05-30 22:14:56 +0900

    feat: 스토리북 세팅

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+module.exports = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        postCss: true,
+      }
+    }
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
