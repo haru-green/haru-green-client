8200c7eccf42eec85bdb8a23b079040f06fa98d5
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-06-10 16:59:27 +0900

    feat: svg 사용 세팅

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack', 'url-loader'],
+    });
+    return config;
+  },
+}
 
 module.exports = nextConfig
