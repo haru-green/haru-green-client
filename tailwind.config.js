da57725ddbde5acd97b074a775f49f36b591e3d7
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-06-11 12:59:34 +0900

    feat: colors 테마 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const colors = {
+  green: '#34AA70',
+  score: {
+    zero: '#000000',
+    one: '#F15252',
+    two: '#FFC700',
+  }
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/app/**/*.tsx'],
   theme: {
-    extend: {},
+    colors
   },
   plugins: [],
 }
